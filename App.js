da78371ca9ab147dad2587f059914d05a9a6d36d
jadu20 <audu1217@naver.com>
2021-02-08 17:12:31 +0900

    Created a new Expo app

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Alert } from "react-native";
+import * as Location from "expo-location";
+import Loading from "./loading";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "b45ba34f7f7833760594c868fc3a1902";
+
+export default class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
+  }
+}
